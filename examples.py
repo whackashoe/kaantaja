207458a08b81291aa34a907aeccfb9dccb207912
Jett LaRue <whackashoe@gmail.com>
2012-03-19 16:55:20 -0500

    update examples

--- examples.py
+++ examples.py
@@ -8,8 +8,9 @@ tarkus = translate.Translator()
 print("Input Language is "+tarkus.detect("hello world"))
 print("Input Language is "+tarkus.detect("bonjour"))
 print("Translate 'bonjour' of language of 'bonjour' to language of 'hello world' "+tarkus.translate("bonjour", tarkus.detect("bonjour"), tarkus.detect("hello world")))
-print("Translate english webpage to french \n"+tarkus.translate("http://docs.python.org/library/codecs.html", "fr"))
+print("Translate english webpage to french \n"+tarkus.translate("http://python3porting.com/intro.html", "fr"))
 
+#translating websites is BUGGY, don't count on it working
 #note that detecting webpages language with the detect function has yet to return any result other than english for me.
 #your best bet with this is extract data from title tag or something and try that
 #also, don't load too many webpages quickly or your appid will be paused for a bit
