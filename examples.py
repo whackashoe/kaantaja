cef03f80b389258da17b731772e7bae605d2d2cb
Jett LaRue <whackashoe@gmail.com>
2012-03-23 22:50:56 -0400

    comment out webpage read

--- examples.py
+++ examples.py
@@ -8,7 +8,7 @@ tarkus = translate.Translator()
 print("Input Language is "+tarkus.detect("hello world"))
 print("Input Language is "+tarkus.detect("bonjour"))
 print("Translate 'bonjour' of language of 'bonjour' to language of 'hello world' "+tarkus.translate("bonjour", tarkus.detect("bonjour"), tarkus.detect("hello world")))
-print("Translate english webpage to french \n"+tarkus.translate("http://python3porting.com/intro.html", "fr"))
+# print("Translate english webpage to french \n"+tarkus.translate("http://python3porting.com/intro.html", "fr"))
 
 #translating websites is BUGGY, don't count on it working
 #note that detecting webpages language with the detect function has yet to return any result other than english for me.
