5f200d46fd689a2400c3d07546f0638251b14ba9
Jett LaRue <whackashoe@gmail.com>
2012-03-24 15:35:01 -0500

    detect is fixed for ambiguity

--- translate.py
+++ translate.py
@@ -148,7 +148,9 @@ class Translator:
 				}
 					
 				finalText.append(self.query(self.detect_api_url, query_args))
-			return finalText
+			
+			
+			return most_common(finalText)
 			
 				
 		query_args = {
@@ -163,7 +165,31 @@ class Translator:
 	#code splits string into n sized pieces
 	def splitCount(self, s, count):
 		return [''.join(x) for x in zip(*[list(s[z::count]) for z in range(count)])]
-		
+	
+	
+	def most_common(lst):
+		cur_length = 0
+		max_length = 0
+		cur_i = 0
+		max_i = 0
+		cur_item = None
+		max_item = None
+		for i, item in sorted(enumerate(lst), key=lambda x: x[1]):
+			if cur_item is None or cur_item != item:
+				if cur_length > max_length or (cur_length == max_length and cur_i < max_i):
+					max_length = cur_length
+					max_i = cur_i
+					max_item = cur_item
+				cur_length = 1
+				cur_i = i
+				cur_item = item
+			else:
+				cur_length += 1
+		if cur_length > max_length or (cur_length == max_length and cur_i < max_i):
+			return cur_item
+		return max_item
+	
+	
 	#attempts to decode items from web
 	def applyDecoding(self, data):
 		if data.startswith(codecs.BOM_UTF8):
